df8a21b03920986466e74913217184ad4c896c61
VhiktorBrown <bukkychukwujekwu@gmail.com>
2021-12-06 15:36:09 +0100

    Made some modifications

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,29 @@
 console.log('A new JavaScript File has been loaded!')
 
-fetch('http://localhost:3000/weather?address=Boston').then((response) => {
+
+const weatherForm = document.querySelector('form')
+
+const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const location = search.value
+
+    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
     response.json().then((data) => {
         if(data.error){
             console.log(data.error)
+            messageOne.textContent = error
         }else{
+            messageTwo.textContent = data.forecastMessage
             console.log(data.forecastMessage)
             console.log(data.address)
         }
     })
+})
+    
+    console.log(location)
 })
